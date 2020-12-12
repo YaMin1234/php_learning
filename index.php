e8bfd2348b74c343b90f52f925f52495c22ac1d0
Ya Min <yamin213811@gmail.com>
2020-12-12 13:38:02 +0630

    ep2. php and html ,getting variable from url using superglobal variable

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            background-color: grey;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1><?= "Hello ".htmlspecialchars($_GET['name']); ?></h1>
+        <!-- getting variable from URL and htmlspecialchars=>checking whether include html tags and then this tags convert to string-->
 
-$greeting = "Hello World";
-echo $greeting;
-
-$name = "yamin";
-echo $name."<br>";
-
-$firstName = "ya";
-echo $firstName.' min'."<br>";
-
-echo '$firstName min'."<br>";//don't work variable within single quote
-
-echo "$firstName min";// work variable within double quote
-
-?>
+</body>
+</html>
