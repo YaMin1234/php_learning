af70e8654dc79a01107d5e4e3f4c82e27c576554
Ya Min <yamin213811@gmail.com>
2020-12-12 13:12:31 +0630

    Declaring Variable

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$greeting = "Hello World";
+echo $greeting;
+
+$name = "yamin";
+echo $name."<br>";
+
+$firstName = "ya";
+echo $firstName.' min'."<br>";
+
+echo '$firstName min'."<br>";//don't work variable within single quote
+
+echo "$firstName min";// work variable within double quote
+
+?>
