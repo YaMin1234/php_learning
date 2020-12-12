51d1f23f4f1b15c76bde76885eb50a9dfa279108
Ya Min <yamin213811@gmail.com>
2020-12-12 15:26:34 +0630

    Declaring Variable

--- index.php
+++ index.php
@@ -1,20 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        h1{
-            background-color: grey;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
-    <h1><?= "Hello ".htmlspecialchars($_GET['name']); ?></h1>
-        <!-- getting variable from URL and htmlspecialchars=>checking whether include html tags and then this tags convert to string-->
+<?php 
 
-</body>
-</html>
+$greeting = "Hello";
+echo $greeting."<br>";
+
+$name = "yamin";
+echo $name."<br>";
+
+$firstName = "ya";
+echo $firstName." min <br>";
+
+echo '$firstName min<br>';//Don't work variable within single quote
+
+echo "$firstName min";//work variable within double quote
+
+?>
