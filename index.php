d9c0c85b6fcc92346a8fa5ae9ff2f9767bae506b
Ya Min <yamin213811@gmail.com>
2020-12-12 15:39:25 +0630

    ep3. Separation of Concern

--- index.php
+++ index.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        h1{
-            background-color: grey;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
-    <h1><?= "Hello ".htmlspecialchars($_GET['name']); ?></h1>
-        <!-- getting variable from URL and htmlspecialchars=>checking whether include html tags and then this tags convert to string-->
-
-</body>
-</html>
+<?php
+$greeting = "Hello";
+require "index.view.php";
+?>
