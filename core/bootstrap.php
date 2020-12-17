9b536dfb2e2bf0526ddba788f3b2138adcfae2c1
Ya Min <yamin213811@gmail.com>
2020-12-17 10:51:32 +0630

    composer (autoloading file)

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,16 +1,18 @@
 <?php
 
-require "core/functions.php";
-$config = require "database/config.php";
-require "core/database/Connection.php";
-require "core/database/QueryBuilder.php";
-require "core/Router.php";
-require "core/Request.php";
+// require "core/functions.php";
+// require "core/database/Connection.php";
+// require "core/database/QueryBuilder.php";
+// require "core/Router.php";
+// require "core/Request.php";
 //$db = Connection::make();
 
+ $config = require "database/config.php";
 $query = new QueryBuilder(
     Connection::make($config['database'])
 );//connection make from connection.php and work query from querybuilder.php or second way=>$db = Connection::make();
                                                                                         //   $query = new QueryBuilder($db);
+
+
 
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php 
 
+require "vendor/autoload.php";
+//requireနဲ့bootstrap မှာ fileတွေကို ခေါ်စရာမလို composerနဲ့ autoload လုပ်ခိုင်းမှာ ပထမ စလုပ်ရမှာက  composer.json fileဆောက် အဲ့ထဲမှာ
+//autoload နဲ့classmapမှာ autoloading တက်ခိုင်းချင်တဲ့အရာအားလုံးကိုရေးရ project file အားလူံးကို လုပ်စေချင်ရင် "./" ပဲရေးရ
+//ပြီးရင် terminal မှာ composer install လို့ရိုက်
+//ပြီးရင် vendor file ပေါ်လာ အဲ့ fileကိုခေါ်ချင်ရင် index.phpမှာ require "vendor/autoload.php" နဲ့ခေါ် 
+//fileတွေထပ်ထည့်ရင် file name နဲ့ class nameတူ‌အောင်ပေးရ
+//ထပ်ထည့်လိုက်တဲ့ file ကအလုပ်မလုပ်သေး အလုပ်လုပ်အောင် terminalမှာ composer dump-autoload ဆိုပြီး ရိုက်‌ပေးရရ 
 require "core/bootstrap.php";
 
 
